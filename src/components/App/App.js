399427ae05fa2f2413d4f62bcba8819d45ce53bc
salsberyscottj <salsberyscottj@johndeere.com>
2021-02-25 15:37:26 -0600

    loaded api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,29 @@
-
+import axios from "axios";
 import { Component } from 'react';
 import './App.css';
 
+const api_key = 'a5b9e8fa25dd4524925eac5e38f665b9';
+
+let data = [];
+let url = 'https://api.twelvedata.com/'
+let stocks = 'symbol=DE,CAT,AAPL,BB'
+
 export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      data
+    }
+  }
+  // https://api.twelvedata.com/time_series?symbol=AAPL&interval=1min&apikey=your_api_key
+  componentDidMount = () => {
+    axios
+      .get(`${url}time_series?${stocks}&interval=1h&apikey=${api_key}`)
+      .then(response => {
+        console.log(response)
+      })
+  }
+  
   render () {
     return (
       <div>Hi</div>
