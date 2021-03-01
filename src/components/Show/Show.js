9384c6fa8840846ae5ff030a3bd427d74ee315f9
mrhaywood <mr.haywood@gmail.com>
2021-03-01 16:58:51 -0600

    conditional logic for price/percentage  differences

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -19,17 +19,26 @@ export default class Show extends Component {
     let differenceSinceLastClose = (theSymbolData.values[0].close - theSymbolData.values[1].close).toFixed(2);
     let percentageSinceLastClose = (differenceSinceLastClose / theSymbolData.values[1].close).toFixed(2);
 
+    let upOrDown = ()=>{
+      if (differenceSinceLastClose > 0 ) {
+        return <div className="green"> ⬆ Δ {differenceSinceLastClose} {percentageSinceLastClose}%</div>
+      }
+      else {
+        return <div className="red"> ⬇ Δ {differenceSinceLastClose} {percentageSinceLastClose}%</div>
+      }
+    }
+
+
     return (
       <div className="show">
         <div className="container">
           <div className="symbol-details">
             All values in {theSymbolData.meta.currency}<br></br>
-            {theSymbolData.meta.symbol} {theSymbolData.values[0].close}  Δ: {differenceSinceLastClose} ({percentageSinceLastClose}%)<br></br>
+            {theSymbolData.meta.symbol} {theSymbolData.values[0].close} {upOrDown()}
               OPEN: {theSymbolData.values[0].open}<br></br>
               HIGH: {theSymbolData.values[0].high}<br></br>
               LOW: {theSymbolData.values[0].low}<br></br>
-
-              PREVIOUS CLOSE:
+              PREVIOUS CLOSE: {theSymbolData.values[1].close}
           </div>
 
         </div>
